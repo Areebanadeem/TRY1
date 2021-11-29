4faf4bfeddb0d748d1fef1869086c764d036fd2e
Areebanadeem <areebanadeem818@gmail.com>
2021-11-29 15:35:39 +0500

    First Commit

---
+++ CLASS/Benefit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CLASS
+{
+    public class Benefit
+    {
+        private string HealthInsrance;
+        private string LifeInsurance;
+        private int vacation;
+
+        public Benefit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Employee Employee
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public double BenefitInLife()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string BenefitInHealth()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int BenefitInVacation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void displayBenefits()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private string HealthInsurance()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private double LifeInsurances()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private int Vacation()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CLASS/Employee.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CLASS
+{
+    public class Employee : iEmployee
+    {
+        private String FirstName;
+        private String LastName;
+        private char gender;
+        private int dependents;
+        private int NumEmployee = 0;
+
+
+        
+        public Employee()
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        public string Employeefname()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string EmployeeLname()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public char EmployeeGen()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int EmployeeDept()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double EmployeeSalary()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double CalculatePay()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DisplayEmployee()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void setNoEmployee()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void getNoEmployee()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private string getfname()
+        {
+            throw new System.NotImplementedException();
+        }
+        private string setfname()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private string getlname()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private string setlname()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private char setGender()
+        {
+            throw new System.NotImplementedException();
+        }
+        private string getGender()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        private int setDependents()
+        {
+            throw new System.NotImplementedException();
+        }
+        private string getDependents()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private double getAnnualSalary()
+        {
+            throw new System.NotImplementedException();
+        }
+        private string setAnnualSalary()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
